8d73f9bff4d2c06a156ea860c7b7e01d4cfdede3
whoisluci <lucmov99@gmail.com>
2025-05-26 22:34:18 +0200

    Finished topItems.js

--- topItems/topItems.js
+++ topItems/topItems.js
@@ -95,6 +95,15 @@ export async function renderTopItems (parentID, limit = 50, offset = 0, timeRang
         .attr('transform', `translate(${margin.left}, 0)`)
         .style('color', '#fff');
 
+    const tooltip = d3.select('body')
+        .append('div')
+        .style('opacity', 0)
+        .attr('class', 'tooltip')
+        .style('background-color', 'var(--white)')
+        .style('border-width', '1px')
+        .style('border-radius', '3px')
+        .style('padding', '10px');
+
     svg.append('g')
         .selectAll('dot')
         .data(topItemsS)
@@ -102,8 +111,37 @@ export async function renderTopItems (parentID, limit = 50, offset = 0, timeRang
         .append('circle')
             .attr('cx', (d, i, nodes) => xScale(d.rank))
             .attr('cy', (d, i, nodes) => yScale(d.popularity))
-            .attr('r', 5.5)
-            .style('fill', 'var(--lilac)');
+            .attr('r', 8)
+            .style('fill', 'var(--lilac)')
+            .on('mouseover', (event, d, i, nodes) => {
+                d3.select('.tooltip')
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+
+                if (!d.album) {
+                    tooltip.html(`
+                        <h3>${d.name}</h3>
+                        <h4>Your ranking: <span class='int'>${d.rank}</span></h4>
+                        <h4>Spotify popularity: <span class='int'>${d.popularity}</span></h4>
+                        <img id='artistImg' class='tooltipImg'src='${d.images[0].url}'>`)
+                    .style("left", (event.pageX + 5) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+                } else {
+                    tooltip.html(`
+                        <h3>${d.name}</h3>
+                        <h4>Your ranking: <span class='int'>${d.rank}</span></h4>
+                        <h4>Spotify popularity: <span class='int'>${d.popularity}</span></h4>
+                        <img id='artistImg' class='tooltipImg'src='${d.album.images[0].url}'>`)
+                    .style("left", (event.pageX + 5) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+                }
+            })
+            .on('mouseout', (d, i, nodes) => {
+                tooltip.transition()
+                .duration(200)
+                .style("opacity", 0);
+            });
 
     const nextBttn = document.createElement('button');
     nextBttn.textContent = 'next graph';
@@ -128,22 +166,63 @@ export async function renderTopItems (parentID, limit = 50, offset = 0, timeRang
 
     typeSel.addEventListener('change', (event) => {
         const type = event.target.value;
-        console.log(type);
+        if (type === 'artists') {
+            changeDataType(timeRange, 'artists', xScale, yScale);
+        } else {
+            changeDataType(timeRange, 'tracks', xScale, yScale);
+        }
     });
 
     timeRngSel.addEventListener('change', (event) => {
         const timeRange = event.target.value;
-        console.log(timeRange);
+        const typeVal = document.querySelector('#timeRngSel').value;
+        
+        if (typeVal === 'tracks') {
+            const topItemsS = STATE.userData.tracks.shortTerm;
+            const topItemsM = STATE.userData.tracks.mediumTerm;
+            const topItemsL = STATE.userData.tracks.longTerm;
 
-        if (timeRange === 'short_range') {
-            changeData(svg, topItemsS, xScale, yScale);
-            console.log(topItemsS); 
-        } else if (timeRange === 'medium_range') {
-            changeData(svg, topItemsM, xScale, yScale);
-        } else{
-            changeData(svg, topItemsL, xScale, yScale);
+            if (timeRange === 'short_range') {
+                changeData(svg, topItemsS, xScale, yScale);
+            } else if (timeRange === 'medium_range') {
+                changeData(svg, topItemsM, xScale, yScale);
+            } else {
+                changeData(svg, topItemsL, xScale, yScale);
+            }
+        } else {
+            if (timeRange === 'short_range') {
+                changeData(svg, topItemsS, xScale, yScale);
+                console.log(topItemsS); 
+            } else if (timeRange === 'medium_range') {
+                changeData(svg, topItemsM, xScale, yScale);
+            } else {
+                changeData(svg, topItemsL, xScale, yScale);
+            }
         }
     });
+}
+
+async function changeDataType (timeRange, type, xScale, yScale) {
+    const topItemsS = await fetchItems(timeRange, type);
+    const topItemsM = await fetchItems('medium_term', type);
+    const topItemsL = await fetchItems('long_term', type);
+
+    STATE.userData.tracks.shortTerm = topItemsS;
+    STATE.userData.tracks.mediumTerm = topItemsM;
+    STATE.userData.tracks.longTerm = topItemsL;
+
+    topItemsS.forEach( (item, i) => {
+        item.rank = i + 1;
+    });
+    topItemsM.forEach( (item, i) => {
+        item.rank = i + 1;
+    });
+    topItemsL.forEach( (item, i) => {
+        item.rank = i + 1;
+    });
+
+    const svg = d3.select('svg');
+    changeData(svg, topItemsS, xScale, yScale);
 }
 
 function changeData (svg, dataset, xScale, yScale) {
